3614ef2223fa377fc0bb0770130901d6fba43903
Peter Pouliot <peter@pouliot.net>
2022-04-26 18:30:49 -0400

    removing dns

--- output.tf
+++ output.tf
@@ -1,12 +1,5 @@
-# =========================================== OUTPUT
 output "AzureVMPublicIPAddresses" {
   value = "${azurerm_public_ip.pip.*.ip_address}"
-}
-output "AzureVM1DNSName" {
-  value = "${element(concat(azurerm_dns_a_record.vm_dns.*.name, list(" ")), 0)}.${element(concat(azurerm_dns_a_record.vm_dns.*.zone_name, list(" ")), 0)}"
-}
-output "AzureVM2DNSName" {
-  value = "${element(concat(azurerm_dns_a_record.vm_dns.*.name, list(" ")), 1)}.${element(concat(azurerm_dns_a_record.vm_dns.*.zone_name, list(" ")), 1)}"
 }
 output "AzureVMSSHPublicKey" {
   value = "${tls_private_key.azure_vms.public_key_openssh}"
