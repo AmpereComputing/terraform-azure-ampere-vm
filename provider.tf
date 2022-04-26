412411777ac1eb0ef5e7b244a82439783c44df0e
Peter Pouliot <peter@pouliot.net>
2022-04-26 19:21:10 -0400

    cleanup

--- provider.tf
+++ provider.tf
@@ -7,7 +7,6 @@ terraform {
 }
 # AzureRM Provider
 provider "azurerm" {
-#    version = "1.23"
     subscription_id = var.subscription_id
     tenant_id = var.tenant_id
     client_id = var.client_id
