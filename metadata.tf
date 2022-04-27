a0099f84495f5e41c2a8ccc814f6ef7c320fc666
Peter Pouliot <peter@pouliot.net>
2022-04-27 12:31:47 -0400

    aligning common code

--- metadata.tf
+++ metadata.tf
@@ -1,5 +1,14 @@
-# Cloud-Config
+# Cloud Init Metadata
+
 data "template_file" "cloud_config" {
-  template = "${file("${path.module}/templates/cloud-config.yml.tpl")}"
+  template = file("${path.module}/templates/cloud-init.yaml.tpl")
+  vars = {
+    tf_ssh_privkey = tls_private_key.azure.private_key_pem
+    tf_ssh_pubkey = tls_private_key.azure.public_key_openssh
+    tf_random_id = random_uuid.random_id.result
+  }
 }
 
+output "cloud_init" {
+  value = data.template_file.cloud_config.rendered
+}
