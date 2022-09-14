d778258ef9c50985723eef6646ee8be0d5f97f75
Peter Pouliot <peter@pouliot.net>
2022-09-14 11:09:48 -0400

    adding forceful delete

--- provider.tf
+++ provider.tf
@@ -7,7 +7,12 @@ terraform {
 }
 # AzureRM Provider
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+# features {}
   subscription_id = var.subscription_id
   tenant_id = var.tenant_id
 }
